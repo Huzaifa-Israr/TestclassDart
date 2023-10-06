6a190a5d5a2eef8b0f828653594add144adaab7a
Huzaifa-Israr <143546157+Huzaifa-Israr@users.noreply.github.com>
2023-10-06 12:12:30 -0700

    myQuiz.application
    My Quiz Application

--- main.dart
+++ main.dart
@@ -1,151 +1,127 @@
 import 'dart:io';
+import 'dart:math';
 
-var mainSelections = "";
-var childSelections = "";
-var restart = false;
+// Map of questions and answers
+Map<int, String> questions = {
+  1: "What is the capital of France?",
+  2: "How many continents are there?",
+  3: "What is the capital of pakisatn?",
+  4: "How many colors are in the rainbow?",
+  5: "Which planet is the hottest?",
+  6: "What color is a ruby?",
+  7: "Which planet is closest to the sun?",
+  8: "How many legs does a spider have?",
+  9: "What is something you hit with a hammer?",
+  10: "What's the name of a place you go to see lots of animals?",
+  11: "Whose nose grew longer every time he lied?",
+  12: "If you freeze water, what do you get?",
+  13: "How many planets are in our solar system?",
+  14: " What do caterpillars turn into?",
+  15: "question",
+  16: "question",
+  17: "question",
+  18: "question",
+  19: "question",
+  20: "question"
+
+  // Add more questions here
+};
+
+Map<int, String> answers = {
+  1: "Paris",
+  2: "7",
+  3: "Islamabad",
+  4: "7",
+  5: "Mercury",
+  6: "Red",
+  7: "Mercury",
+  8: "8",
+  9: "Nail",
+  10: "Zoo",
+  11: "Pinocchio",
+  12: "Ice",
+  13: "8",
+  14: "Butterfuy",
+  15: "Answer",
+  16: "Answer",
+  17: "Answer",
+  18: "Answer",
+  19: "Answer",
+  20: "Answer"
+
+  // Add more answers here
+};
+
+// Function to start the quiz
+void startQuiz() {
+  List<int> randList = generateRandomQuestions();
+  int correctAnswers = 0;
+
+  for (int i = 0; i < 20; i++) {
+    int questionIndex = randList[i];
+    String question = questions[questionIndex];
+    String correctAnswer = answers[questionIndex];
+
+    print("Question ${i + 1}: $question");
+    String userAnswer = getUserInput();
+
+    if (userAnswer.toLowerCase() == correctAnswer.toLowerCase()) {
+      correctAnswers++;
+    }
+  }
+
+  double percentage = (correctAnswers / 20) * 100;
+  displayResult(correctAnswers, percentage);
+}
+
+// Function to generate a list of 20 random question indices
+List<int> generateRandomQuestions() {
+  int max = questions.length;
+  List<int> randList = [];
+  Random random = Random();
+
+  while (randList.length < 20) {
+    int randIndex = random.nextInt(max) + 1;
+    if (!randList.contains(randIndex)) {
+      randList.add(randIndex);
+    }
+  }
+
+  return randList;
+}
+
+// Function to get user input
+String getUserInput() {
+  print("Your Answer: ");
+  return stdin.readLineSync() ?? "";
+}
+
+// Function to display the result
+void displayResult(int correctAnswers, double percentage) {
+  print("\n----- Quiz Results -----");
+  print("Total number of correct answers: $correctAnswers");
+  print("Percentage of correct answers: ${percentage.toStringAsFixed(2)}%");
+
+  if (percentage > 50) {
+    print("Congratulations! You passed the quiz.");
+  } else {
+    print("Better luck next time.");
+  }
+
+  print("\nDo you want to restart the quiz? (yes/no)");
+  String restartChoice = stdin.readLineSync()?.toLowerCase() ?? "";
+
+  if (restartChoice == "yes") {
+    startQuiz();
+  } else {
+    print("Thank you for playing!");
+  }
+}
 
 void main() {
-  do {
-    restart = false;
-    var myOptions = {
-      "1": "Length Conversion",
-      "2": "Temperature Conversion",
-      "3": "Area Conversion",
-      "4": "Weight Conversion",
-      "5": "Time Conversion",
-    };
-    showOptions(myOptions);
-  } while (restart);
-}
+  print("Welcome to the Quiz App!");
+  print("You will be asked 20 random questions.");
+  print("Please answer each question. Type 'quit' to exit at any time.\n");
 
-showOptions(Map optionsMap) {
-  print("---------------------------------");
-  print("Please select one option");
-  for (var i = 1; i <= optionsMap.length; i++) {
-    print("$i: ${optionsMap["$i"]}");
-  }
-
-  mainSelections = stdin.readLineSync()!;
-  if (mainSelections == "1") {
-    showLengthOptions();
-  } else if (mainSelections == "2") {
-    showTempOptions();
-  } else if (mainSelections == "3") {
-  } else if (mainSelections == "4") {
-  } else if (mainSelections == "5") {
-  } else {
-    print("Incorrect Input, Please make selection from above options only");
-    showOptions(optionsMap);
-  }
-}
-
-showLengthOptions() {
-  print("---------------------------------");
-  print("Please select one option");
-  var myOptions = {
-    "1": "Meter to kilometers",
-    "2": "Kilometer to Meters",
-    "3": "feet to Inches",
-    "4": "Inches to Feet",
-    "5": "Centimeter to Meter",
-    "6": "Meter to Centimeter"
-  };
-  for (var i = 1; i <= myOptions.length; i++) {
-    print("$i: ${myOptions["$i"]}");
-  }
-  childSelections = stdin.readLineSync()!;
-  if (childSelections == "1") {
-    meterToKilometers();
-  } else if (childSelections == "2") {
-    kmToMeters();
-  } else if (childSelections == "3") {
-    feetToInches();
-  } else if (childSelections == "4") {
-    inchesToFeet();
-  } else if (childSelections == "5") {
-    cmToMeters();
-  } else if (childSelections == "6") {
-    metersToCm();
-  } else {
-    print("Incorrect Input, Please make selection from above options only");
-    showLengthOptions();
-  }
-  restartProgram();
-}
-
-showTempOptions() {
-  print("---------------------------------");
-  print("Please select one option");
-  var myOptions = {
-    "1": "Fahrenheit to Celcius",
-    "2": "Celcius to Fahrenheit",
-  };
-  for (var i = 1; i <= myOptions.length; i++) {
-    print("$i: ${myOptions["$i"]}");
-  }
-  childSelections = stdin.readLineSync()!;
-  if (childSelections == "1") {
-  } else if (childSelections == "2") {
-  } else {
-    print("Incorrect Input, Please make selection from above options only");
-    showTempOptions();
-  }
-}
-
-//MAIN OPTION NUMBER ->  LENGTH CONVERSION
-//METER to kilometers
-meterToKilometers() {
-  int meters = int.parse(stdin.readLineSync()!);
-  int km = meters ~/ 1000;
-  print("$meters are equal to $km");
-}
-
-//Km to Meters
-kmToMeters() {
-  int km = int.parse(stdin.readLineSync()!);
-  int meter = km * 1000;
-  print("$km are equal to $meter");
-}
-
-//Feet to Inches()
-feetToInches() {
-  int feet = int.parse(stdin.readLineSync()!);
-  int inches = feet * 12;
-  print("$feet are equal to $inches");
-}
-
-//Inches to Feet
-inchesToFeet() {
-  int inches = int.parse(stdin.readLineSync()!);
-  double feet = inches / 12;
-  print("$inches are equal to $feet");
-}
-
-//cm to Meters
-cmToMeters() {
-  int cm = int.parse(stdin.readLineSync()!);
-  double meters = cm / 100;
-  print("$cm are equal to $meters");
-}
-
-//Meters to CM
-metersToCm() {
-  int meter = int.parse(stdin.readLineSync()!);
-  double cm = meter / 100;
-  print("$meter are equal to $cm");
-}
-
-restartProgram() {
-  print("---------------------------------");
-  print("Press R to Continue");
-  print("Press T to Terminate");
-  var input = stdin.readLineSync();
-  if (input == "R") {
-    restart = true;
-  } else if (input == "T") {
-    restart = false;
-  } else {
-    print("Incorrect Input, Please make selection from above options only");
-    restartProgram();
-  }
+  startQuiz();
 }
